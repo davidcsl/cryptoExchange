55737f4c8768006868c2b22cf5ca41c57f87eb90
davidcsl <singaporecsl123@gmail.com>
2024-06-08 02:09:29 +0800

    Added a simple GET end points with sample response

--- src/main/java/com/aquariux/crypto/endpoints/CryptoEndpoints.java
+++ src/main/java/com/aquariux/crypto/endpoints/CryptoEndpoints.java
@@ -1,6 +1,8 @@
 package com.aquariux.crypto.endpoints;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @Slf4j
 public class CryptoEndpoints {
+
+  @GetMapping("price")
+  public ResponseEntity getPrice() {
+    return ResponseEntity.ok("BTC: 70000 USDT, ETH: 3800 USDT");
+  }
 }
