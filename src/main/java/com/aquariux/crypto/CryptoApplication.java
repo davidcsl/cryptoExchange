e92f83ac851b9e3e5908ec25ad5b42a64e990e03
davidcsl <singaporecsl123@gmail.com>
2024-06-08 01:34:40 +0800

    Included New Dependencies in pom.xml Added Controllers Class Comply Check Style

--- src/main/java/com/aquariux/crypto/CryptoApplication.java
+++ src/main/java/com/aquariux/crypto/CryptoApplication.java
@@ -3,11 +3,10 @@ package com.aquariux.crypto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Crypto spring boot application entry point. */
 @SpringBootApplication
 public class CryptoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CryptoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CryptoApplication.class, args);
+  }
 }
---
+++ src/main/java/com/aquariux/crypto/endpoints/CryptoEndpoints.java
@@ -0,0 +1,14 @@
+package com.aquariux.crypto.endpoints;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Crypto RESTFul endpoint controller.
+ */
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class CryptoEndpoints {
+}
